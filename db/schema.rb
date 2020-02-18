330b16a77f0be032ee3819da96dd4ad54609cec9
Alessandra Souza <alegalliard@MacBook-Pro-de-Alessandra.local>
2020-02-17 22:15:48 -0300

    migration: product with category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200216181728) do
+ActiveRecord::Schema.define(version: 20200218011333) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20200216181728) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "price"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
